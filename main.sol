95e94b2744576bea5c755c36204cd16d08ae1121
ysur68522 <ysur68522@gmail.com>
2026-03-05 18:50:31 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -68,3 +68,73 @@ contract Jer0me is ReentrancyGuard, Pausable, Ownable {
 
     event TerminalSessionClosed(uint256 indexed sessionId, uint256 atBlock);
 
+    event RelayFeedUpdated(
+        uint256 indexed feedIndex,
+        int256 value,
+        uint256 timestamp,
+        uint256 atBlock
+    );
+
+    event TreasurySweep(address indexed token, address indexed to, uint256 amount, uint256 atBlock);
+
+    event RelayRotated(address indexed previousRelay, address indexed newRelay, uint256 atBlock);
+
+    event EpochAdvanced(uint256 indexed previousEpoch, uint256 newEpoch, uint256 atBlock);
+
+    event GuardianPauseToggled(bool paused, uint256 atBlock);
+
+    event BandCapSet(uint256 previousCap, uint256 newCap, uint256 atBlock);
+
+    event AnalystWhitelistSet(address indexed analyst, bool allowed, uint256 atBlock);
+
+    event StaleWindowSet(uint256 previousWindow, uint256 newWindow, uint256 atBlock);
+
+    event FeeBpsSet(uint256 previousBps, uint256 newBps, uint256 atBlock);
+
+    event FallbackReceiverSet(address indexed previous, address indexed current, uint256 atBlock);
+
+    // -------------------------------------------------------------------------
+    // ERRORS
+    // -------------------------------------------------------------------------
+
+    error J0R_NotRelay();
+    error J0R_NotGuardian();
+    error J0R_NotAnalyst();
+    error J0R_ZeroAddress();
+    error J0R_ZeroAmount();
+    error J0R_BandNotFound();
+    error J0R_BandInactive();
+    error J0R_BandBoundsInvalid();
+    error J0R_SessionExpired();
+    error J0R_SessionNotOpen();
+    error J0R_StaleFeed();
+    error J0R_DirectionInvalid();
+    error J0R_CapExceeded();
+    error J0R_EpochNotAdvanced();
+    error J0R_TransferFailed();
+    error J0R_ArrayLengthMismatch();
+    error J0R_EmptyArray();
+    error J0R_Reentrancy();
+    error J0R_Paused();
+    error J0R_FeeBpsTooHigh();
+    error J0R_StaleWindowTooLarge();
+
+    // -------------------------------------------------------------------------
+    // CONSTANTS
+    // -------------------------------------------------------------------------
+
+    uint256 public constant JER0ME_VERSION = 2;
+    bytes32 public constant JER0ME_NAMESPACE = keccak256("Jer0me.fed.v2");
+    uint256 public constant BPS_DENOMINATOR = 10_000;
+    uint256 public constant MAX_FEE_BPS = 500;
+    uint256 public constant MAX_STALE_BLOCKS = 200;
+    uint256 public constant MIN_BAND_GAP = 1;
+    uint256 public constant MAX_BANDS_DEFAULT = 64;
+    uint256 public constant VOTE_DIRECTION_HOLD = 0;
+    uint256 public constant VOTE_DIRECTION_UP = 1;
+    uint256 public constant VOTE_DIRECTION_DOWN = 2;
+    uint256 public constant SESSION_DURATION_BLOCKS = 150;
+    uint256 public constant MAX_FEEDS = 16;
+    uint256 public constant BATCH_SIGNALS_MAX = 32;
+    uint256 public constant EPOCH_BLOCKS = 2016;
+
